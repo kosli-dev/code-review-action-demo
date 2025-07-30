ed263392d199770fc49d46310091f298df32d476
Tore Martin Hagen <tore@kosli.com>
2025-07-30 08:46:00 +0200

    Updated demo script with release

--- scripts/demo-code-review.sh
+++ scripts/demo-code-review.sh
@@ -69,12 +69,31 @@ function update_content_file
     grep "counter=" ${file} | sed "s/counter=//"
 }
 
+function wait_for_pr_approval
+{
+    echo
+    echo "*** Approve the PR in github ***"
+    echo "After that press 'c' to continue"
+    while :; do
+      read -n 1 key
+      if [[ "$key" == "c" ]]; then
+        echo -e "\nContinuing..."
+        break
+      fi
+    done
+}
+
+function increment_version {
+    local current_tag="$1"
+    echo "$current_tag" | sed -E 's/([0-9]+)$/echo $((\1+1))/e'
+}
+
 main()
 {
     check_arguments "$@"
 
-    echo; echo "*** Create a branch, update source and make a pull-request"
-    git checkout -b ${BRANCH_NAME}-demo
+    echo; echo "*** Create a branch, update source and make a squash pull-request"
+    git checkout -b ${BRANCH_NAME}-demo-1
     FE_VER=$(update_content_file src/frontend.txt)
     git add src
     git commit -m "${BRANCH_NAME} Updated SW frontend=${FE_VER}"
@@ -84,61 +103,32 @@ main()
     git push; wait_for_github_actions
     gh pr create --fill
 
+    wait_for_pr_approval
+    gh pr merge --auto --squash --delete-branch --subject "${BRANCH_NAME} Updated SW frontend=${FE_VER}"
+    wait_for_github_actions
 
+    echo; echo "*** Create a branch, update source and make a merge pull-request"
+    git checkout -b ${BRANCH_NAME}-demo-2
+    FE_VER=$(update_content_file src/frontend.txt)
+    git add src
+    git commit -m "${BRANCH_NAME} Updated SW frontend=${FE_VER}"
+    FE_VER=$(update_content_file src/frontend.txt)
+    git add src
+    git commit -m "${BRANCH_NAME} Updated SW frontend=${FE_VER}"
+    git push; wait_for_github_actions
+    gh pr create --fill
 
+    wait_for_pr_approval
+    gh pr merge --auto --merge --delete-branch; wait_for_github_actions
 
-#    echo; echo "*** Waiting for pull request to do required checks before merge"; wait_for_github_actions
-#    gh pr merge --auto --squash --delete-branch; wait_for_github_actions
-#    echo; echo "*** Wait for build on main to finish"; wait_for_github_actions
-#    make report_all_envs > /dev/null; wait_for_github_actions
-#    echo; echo "*** SW is now running in dev. Do a deployment from dev to staging"
-#    make deploy_to_staging; wait_for_github_actions
-#    make report_all_envs > /dev/null; wait_for_github_actions
-#    echo; echo "*** Make a release candidate for SW now running in staging with Jira issue ${JIRA_KEY_1}"
-#    make generate_jira_release; wait_for_github_actions
-#
-#    echo; echo "*** We assume the product owner found a bug and wanted a new version of the backend"
-#
-#    echo; echo "*** Create a branch, update backend app and make a pull-request **"
-#    git checkout -b ${JIRA_KEY_2}-demo-2
-#    BE_VER=$(update_content_file apps/backend/backend-content.txt)
-#    git add apps/
-#    git commit -m "${JIRA_KEY_2} Updated SW backend=${BE_VER}"
-#    git push; wait_for_github_actions
-#    gh pr create --fill
-#    echo; echo "*** Waiting for pull request to do required checks before merge"; wait_for_github_actions
-#    gh pr merge --auto --squash --delete-branch; wait_for_github_actions
-#    echo; echo "*** Wait for build on main to finish"; wait_for_github_actions
-#    make report_all_envs > /dev/null; wait_for_github_actions
-#    echo; echo "*** Updated SW is now running in dev. Do a deployment from dev to staging"
-#    make deploy_to_staging; wait_for_github_actions
-#    make report_all_envs > /dev/null; wait_for_github_actions
-#    echo; echo "*** Update the release candidate for SW now running in staging."
-#    echo "*** This will add the second JIRA-KEY ($JIRA_KEY_2) to the Jira release"
-#    make generate_jira_release; wait_for_github_actions
-#    echo; echo "*** Check if current release candidate has been approved and can be released. This shall fail!"
-#    make check_release_to_prod; wait_for_github_actions
-#
-#    echo; echo "*** Go to url:"
-#    echo "https://kosli-team.atlassian.net/projects/OPS?selectedItem=com.atlassian.jira.jira-projects-plugin%3Arelease-page"
-#    echo
-#    echo "Press the version you see in the list. It should only be one that is UNRELEASED"
-#    echo "On the right hand side press the + next to Approvers"
-#    echo "Add your self as an approver"
-#    echo "Change the approval from PENDING to APPROVED"
-#    echo "After that press 'c' to continue"
-#    while :; do
-#      read -n 1 key
-#      if [[ "$key" == "c" ]]; then
-#        echo -e "\nContinuing..."
-#        break
-#      fi
-#    done
-#    echo; echo "*** Check if release has been approved"
-#    make check_release_to_prod; wait_for_github_actions
-#    make report_all_envs > /dev/null; wait_for_github_actions
-#    make update_tags
-#    echo; echo "*** You can now check kosli UX to see that correct SW is running and that attestations have been done"
+    echo; echo "*** Create new version tag and push ***"
+    CURRENT_TAG=$(git describe --tags --abbrev=0)
+    NEXT_TAG=$(increment_version ${CURRENT_TAG})
+    git tag -a ${NEXT_TAG} -m "Version ${NEXT_TAG}"
+    git push origin ${NEXT_TAG}
+    wait_for_github_actions
+
+    echo; echo "*** Release has now been created and reported to kosli ***"
 }
 
 main "$@"
